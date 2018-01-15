1d4298eb22d4670b73bc76d438a17e9cb3a3bf3a
abeauvoi <abeauvoi@student.42.fr>
2018-01-15 08:42:48 +0100

    git sh - abeauvoi - 15-01-08h42m - e1r4p13.42.fr

--- src/conv/ft_atoi.c
+++ src/conv/ft_atoi.c
@@ -6,37 +6,49 @@
 /*   By: abeauvoi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/15 17:31:53 by abeauvoi          #+#    #+#             */
-/*   Updated: 2018/01/10 23:54:32 by abeauvoi         ###   ########.fr       */
+/*   Updated: 2018/01/15 08:42:17 by abeauvoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <libft.h>
 
-static unsigned int	digit_value(char c)
+static const char	*prelim_check(const char *s, t_bool *neg)
 {
-	return ((unsigned int)(c - '0'));
+	while (ft_isspace(*s))
+		++s;
+	if (*s == '-')
+	{
+		*neg = TRUE;
+		++s;
+	}
+	else if (*s == '+')
+		++s;
+	return (s);
 }
 
 int					ft_atoi(const char *s)
 {
 	t_bool			neg;
 	unsigned int	acc;
-	unsigned int	d;
+	unsigned int	digit;
 
-	acc = 0;
 	neg = FALSE;
-	while (ft_isspace(*s))
-		++s;
-	if (*s == '-')
+	s = prelim_check(s, &neg);
+	if (!((acc = TO_DIGIT(*s)) <= 9))
+		return (0);
+	while ((digit = TO_DIGIT(*++s)) <= 9)
 	{
-		neg = TRUE;
-		++s;
+		if (acc > INT_MAX / 10)
+			return (-1);
+		else
+		{
+			acc *= 10;
+			if (INT_MAX - digit < acc)
+				return (-1);
+			else
+				acc += digit;
+		}
 	}
-	else if (*s == '+')
-		++s;
-	acc = digit_value(*s);
-	while ((d = digit_value(*++s)) <= 9)
-		acc = acc * 10 + d;
 	if (neg)
 		acc = ~acc + 1;
 	return ((int)acc);
